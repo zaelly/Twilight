490c91a3819f149cc17f5a4f89a338c041209668
Zaelly Barbosa <116820537+zaelly-oliveira@users.noreply.github.com>
2024-04-04 17:33:27 -0300

    input search com btn e enter pegando o valor setad

--- twilight-project/src/components/Search.jsx
+++ twilight-project/src/components/Search.jsx
@@ -5,14 +5,23 @@ const Search = () => {
 
   const [inputBtn, setInputBtn] = useState('');
 
-  const  handleSubmit = (e) => {
-    e.target.value = setInputBtn;
-    console.log(inputBtn);
+  const handleEnter = (e)=>{
+    if(e.key == "Enter"){
+      console.log("Pesquisa:", inputBtn)
+    }
+  }
+
+  const  handleSubmit = () => {
+    console.log("Pesquisa:", inputBtn)
+  }
+
+  const handleChange = (e)=>{
+    setInputBtn(e.target.value);
   }
 
   return (
     <div className='container-Search'>
-      <input className='input-Search' type="text" name='search' placeholder='Pesquisar...' />
+      <input className='input-Search' type="text" name='search' placeholder='Pesquisar...' onChange={handleChange} onKeyDown={handleEnter}/>
       <button type="submit" className='btn-search' onClick={handleSubmit}>
         <span className='icon-span icon-search'>
           <i className="fa-solid fa-magnifying-glass"></i>
