86dde63a6749b52f123773e445b6fb85817f7018
Zaelly Barbosa <zazabarbosa2@gmail.com>
2024-10-12 22:53:23 -0300

    new add

--- twilight-project/src/pages/home/Home.jsx
+++ twilight-project/src/pages/home/Home.jsx
@@ -1,11 +1,78 @@
-import Carosel from "../../components/carrosel/Carosel"
-
+// import Carosel from '../../components/carrosel/Carosel';
 import './index.css';
+// import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
 const Home = () => {
   return (
-    <div className="mainContainer">
-      <Carosel />
+    <div className="container text-center">
+      <div className="slideShow">
+        <div className="slideShowInner">
+
+        </div>
+      {/* <Carosel/> */}
+      </div>
+      <Container>
+        <Row className='Row'>
+          <h2 className='titlePrin'>Populares</h2>
+            <h3 className='titlePop'>Animes</h3>
+            <div className='caards'>
+              <Card style={{ width: '7rem', height: '9rem', background:"white"}}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+            </div>
+        </Row>  
+        <Row className='Row'>
+            <h3 className='titlePop'>Filmes</h3>
+            <div className='caards'>
+              <Card style={{ width: '7rem', height: '9rem', background:"white"}}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>              <Card style={{ width: '7rem', height: '9rem', background:"white" }}>
+                <Card.Body style={{ background:"none" }}>
+                </Card.Body>
+              </Card>
+            </div>
+        </Row>
+      </Container>
     </div>
   );
 };
